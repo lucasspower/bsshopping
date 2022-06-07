b911c5bda1ba8d21bd10615fb279ac863559818d
lucasspower <lucassilvapower@gmail.com>
2022-06-07 08:33:06 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,88 +1,112 @@
-// https://demo.minotheme.com/zangi/html/?i=243246&aff=entheos
-let allProdutos = document.querySelectorAll(".produto-item");
+let todosProdutos = document.querySelectorAll(".produto-item");
+// Produtos do carrinho
+const cartProdutos = document.querySelector(".grid-cart-produtos");
+const urlApi = "https://fakestoreapi.com/products?limit=*";
 
-const result = fetch("https://fakestoreapi.com/products?limit=5")
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((json) => {
-    return json;
-  });
-result.then((result) => {
-  createItemProduto(result);
-  allProdutos = document.querySelectorAll(".produto-item");
-});
-function createItemProduto(ArrProdutos) {
-  ArrProdutos.forEach((products) => {
-    criarElemento(products);
-  });
+async function initFetchApi(api) {
+  const response = await fetch(api);
+  const result = await response.json();
+  criarItemProduto(result);
+  todosProdutos = document.querySelectorAll(".produto-item");
 }
-// BTN CARRINHO
+initFetchApi(urlApi);
 
-function criarElemento(products) {
-  const itemProduto = document.createElement("section");
-  itemProduto.classList.add("produto-item");
-  itemProduto.innerHTML = `
-      <span class="produto-item-categoria">${products.category}</span>
-      <img class="produto-item-img" src="${products.image}" />
-      <h2 class="produto-item-title">${products.title}</h2>
-      <p class="produto-item-descripton"> 
-      ${formateItemProduto(products.description)}
-      </p>
-      <span class="produto-item-price">$ ${products.price}</span>
-      <span class="produto-item-carrinho">
-      <i class="fa-solid fa-cart-plus"></i>
-      </span>
-    `;
-  appendProduto(itemProduto);
-  extendDescription(products.description);
-  const btnCarrinho = document.querySelectorAll(".produto-item-carrinho");
-  btnCarrinho.forEach((btn) => {
-    btn.addEventListener("click", addCarinhoItem);
-  });
-}
-
-const containerProdutos = document.querySelector(".container-produtos");
-function appendProduto(produto) {
-  containerProdutos.appendChild(produto);
-}
-
-function formateItemProduto(description) {
+// descrição do produto com até 20 caracteres
+const formatarDescricaoProduto = function (description) {
   const textFormate =
-    description.slice(0, 20) +
+    description.slice(0, 30) +
     "..." +
     '<a class="btn-ler-mais" >ler mais...</a>';
-  // return `${textFormate} ${btnLerMais.innerHTML}`;
-
   return textFormate;
+};
+
+// cria um card para cada produto e o adiciona no container
+function criarItemProduto(listaProdutos) {
+  const containerProdutos = document.querySelector(".container-produtos");
+
+  listaProdutos.forEach((produto) => {
+    const itemProduto = document.createElement("section");
+    itemProduto.classList.add("produto-item");
+    itemProduto.innerHTML = `
+      <span class="produto-item-categoria">
+        ${produto.category}
+      </span>
+      <img class="produto-item-img" src="${produto.image}" />
+      <h2 class="produto-item-title">
+      ${produto.title}
+      </h2>
+      <p class="produto-item-descripton"> 
+        ${formatarDescricaoProduto(produto.description)}
+      </p>
+      <span class="produto-item-price">$ 
+        ${produto.price}
+      </span>
+      <button class="produto-item-carrinho">
+        Comprar 
+        <i class="fa-solid fa-cart-plus"></i>
+      </button>
+    `;
+    containerProdutos.appendChild(itemProduto);
+    // descricaoProduto(produto.description);
+  });
+
+  // encurtar a descrição do produto e adicinar enveto de adicionar no carrinho
+  const btnCarrinho = document.querySelectorAll(".produto-item-carrinho");
+  btnCarrinho.forEach((btn) => {
+    btn.addEventListener("click", addProdutoCarrinho);
+  });
 }
 
-function extendDescription(descriptionAll) {
+// Adiciona evento no botao de comprar
+const btnCarrinho = document.querySelectorAll(".produto-item-carrinho");
+btnCarrinho.forEach((btn) => {
+  btn.addEventListener("click", addProdutoCarrinho);
+});
+
+// Mostra toda a descriçao qunado clicar no botao "ler mais"
+function descricaoProduto(descriptionAll) {
   const btnsLerMais = document.querySelectorAll(".btn-ler-mais");
   btnsLerMais.forEach((btnLerMais) => {
     btnLerMais.addEventListener("click", (e) => {
-      btnLerMais.parentElement.innerText = descriptionAll;
+      e.currentTarget.parentElement
+        ? (e.currentTarget.parentElement.innerText = descriptionAll)
+        : null;
     });
   });
 }
 
-function addCarinhoItem(item) {
-  // console.log(item.path);
-  const title = item.path[2].querySelector(".produto-item-title").innerText;
-  const price = item.path[2]
+// Abrir a scola de produtos
+const sacolaProdutos = document.querySelector(".bagFinish");
+const showCart = function () {
+  document.body.classList.add("desable");
+  cartProdutos.parentElement.classList.add("active");
+};
+sacolaProdutos.addEventListener("click", showCart);
+
+// Adiciona o produto no carrinho de compras
+function addProdutoCarrinho(item) {
+  const produtoSelecionado = item.currentTarget.parentElement;
+  const title = produtoSelecionado.querySelector(
+    ".produto-item-title"
+  ).innerText;
+
+  const price = produtoSelecionado
     .querySelector(".produto-item-price")
     .innerText.replace(" ", "");
-  const image = item.path[2].querySelector(".produto-item-img").src;
+
+  const image = produtoSelecionado.querySelector(".produto-item-img").src;
 
   const allItensCart = document.querySelectorAll(".grid-cart-produtos > div");
-  let adicionado = false;
+
+  let produtoAdicionado = false;
+
   Array.from(allItensCart).forEach((item) => {
     if (item.querySelector("h3").innerText == title) {
       const quantidade = item.querySelector(".valueQntdd");
       quantidade.value = +quantidade.value + 1;
-      adicionado = true;
+      produtoAdicionado = true;
       showCart();
-      calcPrice();
+      calcularPreco();
     }
   });
   const objDadosProdutos = {
@@ -90,152 +114,132 @@ function addCarinhoItem(item) {
     price,
     image,
   };
-  !adicionado ? addProdutoCart(objDadosProdutos) : null;
+  produtoAdicionado ? null : addProdutoCart(objDadosProdutos);
   removeProdutos();
-  // const quantidade = document.querySelector(".valueQntdd");
-  // quantidade.addEventListener("change", calcPrice);
 }
+
+// Calcula o preço de todos os produtos do carrinho
+const precoTotal = document.querySelector(".valorTotal");
+const calcularPreco = function () {
+  const itensProdutos = document.querySelectorAll(".cart-produto-item");
+  const valorTotal = Array.from(itensProdutos).reduce((acc, produto) => {
+    let precoProduto = produto
+      .querySelector(".price")
+      .innerText.replace(",", ".");
+    const quantidadeProduto = produto.querySelector(".valueQntdd");
+    precoProduto = +precoProduto.replace("$", "");
+    precoProduto *= quantidadeProduto.value;
+    return acc + precoProduto;
+  }, 0);
+  precoTotal.innerText = `$${valorTotal.toFixed(2)}`;
+};
+
+// Adiciona o produto no carrinho
 function addProdutoCart(produto) {
   const formateProduto = document.createElement("div");
+  formateProduto.classList.add("cart-produto-item");
   formateProduto.innerHTML = `
-  <div class="cart-produto-item">
-    <img src="${produto.image}" alt="${produto.title}"/>
-    <h3>${produto.title}</h3>
-    <span class="price">${produto.price}</span>
-    <span class="remove-produto">X</span>
-    <p class="qntddProduto">Quantidade:<input type="number" name="quantidade" class="valueQntdd" value="1" /></p>
-  </div>
-  <hr />
+      <img src="${produto.image}" alt="${produto.title}"/>
+      <h3>${produto.title}</h3>
+      <span class="price">
+        ${produto.price}
+      </span>
+      <span class="remove-produto">X</span>
+      <p class="qntddProduto">
+        Quantidade:<input type="number" name="quantidade" class="valueQntdd" value="1" />
+      </p>
   `;
 
   cartProdutos.appendChild(formateProduto);
   showCart();
-  calcPrice();
-  const inputsQuantidade = document.querySelectorAll(".valueQntdd");
-  inputsQuantidade.forEach((item) => {
-    item.addEventListener("change", calcPrice);
-  });
+  calcularPreco();
+
+  // Calcula a quantidade de produtos selecionada
+  const inputQuantidadeProd = Array.from(
+    document.querySelectorAll(".valueQntdd")
+  ).at(-1);
+  inputQuantidadeProd.addEventListener("change", calcularPreco);
 }
 
-const btnCarrinho = document.querySelectorAll(".produto-item-carrinho");
-btnCarrinho.forEach((btn) => {
-  btn.addEventListener("click", addCarinhoItem);
-});
-
-const cartProdutos = document.querySelector(".grid-cart-produtos");
-
-const precoTotal = document.querySelector(".valorTotal");
-function calcPrice() {
-  const itensProdutos = document.querySelectorAll(".cart-produto-item");
-  const valorTotal = Array.from(itensProdutos).reduce((acc, prod) => {
-    let priceProduto = prod.querySelector(".price").innerText.replace(",", ".");
-    const qntddProd = prod.querySelector(".valueQntdd");
-    priceProduto = +priceProduto.replace("$", "");
-    priceProduto *= qntddProd.value;
-    // console.log(qntddProd.value);
-    return acc + priceProduto;
-  }, 0);
-
-  precoTotal.innerText = `$${valorTotal.toFixed(2)}`;
-}
-
-// FUNCAO PARA BUSCA SEARCH
-allProdutos = document.querySelectorAll(".produto-item");
-const inputSearch = document.getElementById("search");
-
-const normalizeText = function (value) {
-  return value
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "");
-};
-
-function handleKeypress(e) {
-  allProdutos.forEach((produto) => {
-    const titleProduto = produto.querySelector(".produto-item-title");
-    if (
-      normalizeText(titleProduto.innerText).includes(
-        normalizeText(inputSearch.value)
-      )
-    ) {
-      produto.classList.contains("inativeSearch")
-        ? produto.classList.remove("inativeSearch")
-        : null;
-    } else produto.classList.add("inativeSearch");
-    produtoNaoEncontrado();
-  });
-}
-
-inputSearch.addEventListener("keyup", handleKeypress);
-
-// NUNHUM PRODUTO ENCONTRADO
-/*
-function zeroProdutos() {
-  const resulZeroProdutos = Array.from(allProdutos).every((produto) => {
-    return produto.classList.contains("inativeSearch");
-  });
-  resulZeroProdutos ? console.log("ZEOO") : null;
-}
-*/
-//CLOSET CART
-const btnFechar = document.querySelector(".btn-fechar-cart");
-const btnContinuarComprando = document.querySelector(
-  ".btn-continuar-comprando"
-);
-btnFechar.addEventListener("click", handleClickCloseCart);
-btnContinuarComprando.addEventListener("click", handleClickCloseCart);
-
-function handleClickCloseCart(e) {
-  cartProdutos.parentElement.classList.remove("active");
-  document.body.classList.remove("desable");
-}
-
-// REMOVE PRODUTO;
-function removeProdutos() {
+// Remove o produto do carrinho quando clicar no "X"
+const removeProdutos = function () {
   const btnsRemoveProduto = document.querySelectorAll(".remove-produto");
   btnsRemoveProduto.forEach((btn) => {
-    btn.addEventListener("click", removerProduto);
+    btn.addEventListener("click", (e) => {
+      e.target.parentElement.remove();
+      calcularPreco();
+    });
   });
-}
+};
 
-function removerProduto(e) {
-  e.path[2].remove();
-  calcPrice();
-}
-
-// TESTE OBSERVE
-// const itemProduto = document.querySelectorAll(".produto-item")[0];
-// const observar = new MutationObserver(() => {
-//   console.log(this);
-// });
-// observar.observe(itemProduto, {
-//   attributeFilter: ["class"],
-// });
-
+// Informa que o produto buscado não foi encontrado
 const titleSemProduto = document.querySelector(".sem-produtos");
-function produtoNaoEncontrado() {
-  const result = Array.from(allProdutos).every((produto) => {
+const produtoNaoEncontrado = function () {
+  const result = Array.from(todosProdutos).every((produto) => {
     return produto.classList.contains("inativeSearch");
   });
   result
     ? titleSemProduto.classList.add("active")
     : titleSemProduto.classList.remove("active");
-}
+};
 
-// FORMATANDO A BAG
-const sacolaProdutos = document.querySelector(".bagFinish");
-sacolaProdutos.addEventListener("click", showCart);
-function showCart() {
-  document.body.classList.add("desable");
-  cartProdutos.parentElement.classList.add("active");
-}
+// Buscar os produtos / Search
+const initSearch = function () {
+  todosProdutos = document.querySelectorAll(".produto-item");
+  const inputSearch = document.getElementById("search");
 
-const btnHamburguer = document.querySelector(".btn-hamburguer");
+  // Remove acentos do texto
+  const normalizeText = function (value) {
+    return value
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+  };
 
-btnHamburguer.addEventListener("click", handleMenuMobile);
-function handleMenuMobile(e) {
-  btnHamburguer.classList.toggle("active");
-  menuHeader.classList.toggle("active");
-}
+  const handleKeypress = function (e) {
+    todosProdutos.forEach((produto) => {
+      const titleProduto = produto.querySelector(".produto-item-title");
+      if (
+        normalizeText(titleProduto.innerText).includes(
+          normalizeText(inputSearch.value)
+        )
+      ) {
+        produto.classList.contains("inativeSearch")
+          ? produto.classList.remove("inativeSearch")
+          : null;
+      } else produto.classList.add("inativeSearch");
+      produtoNaoEncontrado();
+    });
+  };
 
-const menuHeader = document.querySelector(".menu-header");
+  inputSearch.addEventListener("keyup", handleKeypress);
+};
+initSearch();
+
+//Fechar o carrinho de compras ou continuar comprando
+const intiFecharCarrinho = function () {
+  const btnFechar = document.querySelector(".btn-fechar-cart");
+  const btnContinuarComprando = document.querySelector(
+    ".btn-continuar-comprando"
+  );
+  btnFechar.addEventListener("click", handleClickCloseCart);
+  btnContinuarComprando.addEventListener("click", handleClickCloseCart);
+
+  function handleClickCloseCart(e) {
+    cartProdutos.parentElement.classList.remove("active");
+    document.body.classList.remove("desable");
+  }
+};
+intiFecharCarrinho();
+
+// Botão mobile - Menu mobile
+const initBtnMobile = function () {
+  const btnHamburguer = document.querySelector(".btn-hamburguer");
+  const menuHeader = document.querySelector(".menu-header");
+  btnHamburguer.addEventListener("click", handleMenuMobile);
+  function handleMenuMobile(e) {
+    btnHamburguer.classList.toggle("active");
+    menuHeader.classList.toggle("active");
+  }
+};
+initBtnMobile();
